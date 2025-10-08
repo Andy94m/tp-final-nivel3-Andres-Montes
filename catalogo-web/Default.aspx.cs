d384eb5b115399598beee1a959fe8260ec7344b1
Andy94m <m.andresmontes@gmail.com>
2025-10-08 17:43:42 -0300

    conexión listar

--- catalogo-web/Default.aspx.cs
+++ catalogo-web/Default.aspx.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace catalogo_web
 {
     public partial class Default : System.Web.UI.Page
     {
+        public List<Articulo> ListaArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            ListaArticulos = negocio.listarConSP();
 
+            if (!IsPostBack)
+            {
+                repRepetidor.DataSource = ListaArticulos;
+                repRepetidor.DataBind();
+            }
         }
+
+
+
+        
+
     }
 }
